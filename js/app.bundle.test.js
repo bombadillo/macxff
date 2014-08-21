ac7960c3ec2c33bacc1e63324c6e5f6ac476205f
Chris Mumford <mumfordnsons@gmail.com>
2014-08-21 12:36:56 +0100

    Adding tests for frontend server fetch
    Testing if successful/failed calls are detected.

--- js/app.bundle.test.js
+++ js/app.bundle.test.js
@@ -14,6 +14,105 @@ var $         = require('jquery'),
     models    = require('./models/global'),
     qunit     = require('./_vendor/qunit');
 
+
+/**
+ * Set the module for the following tests
+ */
+QUnit.module('Fetch Feeds');
+
+
+// Test that when we send a valid feed name, we get data back
+test("Pass-Check: Server fetch with valid feed name", 1, function () {
+
+	// Object containing our test data
+	var oTests = {
+		0: { 
+			params:  'topstories',
+			testMessage: 'We should have data for this valid name'
+		},
+	};
+
+	// Set array to contain each collection
+	var aCollections = [];
+
+	// Loop for each test in array
+	for (var i in oTests) {
+
+	    // Stop QUnit
+	    stop(); 		
+
+		// Get the current test
+		var oTest = oTests[i];
+
+		// Define instance of collection
+		aCollections[i] = new models.BasicCollection();
+		aCollections[i].params = oTest.params;
+		aCollections[i].testMessage = oTest.testMessage;
+		aCollections[i].urlRoot = globals.api.getFeed;
+
+	    // Make call to server
+	    aCollections[i].fetch({
+	        success: function (collection, response) {
+
+	        	// Check that we've got data
+	        	var bGotData = collection.length > 0;
+
+	        	// Check that we have a collection with data
+	        	ok(bGotData, collection.testMessage);
+
+	        	// Start QUnit
+	        	start();
+	        }
+	    });
+	    // END fetch
+	}
+	// END loop
+});
+
+// Let's make sure we get errors for submitting these values
+test("Fail-Check: Server conversion to numerals", 1, function () {
+
+	// Object containing our test data
+	var oTests = {
+		0: { 
+			params:  'topstoriessdgsg',
+			testMessage: 'We should not have data for this invalid name'
+		},
+	}; 
+
+	// Set array to contain each collection
+	var aCollections = [];
+
+	// Loop for each test in array
+	for (var i in oTests) {
+
+	    // Stop QUnit
+	    stop(); 		
+
+		// Get the current test
+		var oTest = oTests[i];
+
+		// Define instance of collection
+		aCollections[i] = new models.BasicCollection();
+		aCollections[i].params = oTest.params;
+		aCollections[i].testMessage = oTest.testMessage;
+		aCollections[i].urlRoot = globals.api.getFeed;
+
+	    // Make call to server
+	    aCollections[i].fetch({
+	        success: function (collection, response) {
+
+	        	// Check that collection.errors is true
+	        	ok(collection.errors, collection.testMessage);
+
+	        	// Start QUnit
+	        	start();
+	        }
+	    });
+	    // END fetch
+	}
+	// END loop
+});
 },{"./_vendor/qunit":1,"./common/eventChannel":3,"./common/globals":4,"./common/router":5,"./common/utilities":6,"./models/global":7,"backbone":14,"jquery":15}],3:[function(require,module,exports){
 /* Require modules */
 var $        = require('jquery'),
