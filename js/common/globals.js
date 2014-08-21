f5b572ea09817db7f5d39887a25cd1038ce14951
Chris Mumford <mumfordnsons@gmail.com>
2014-08-21 11:58:56 +0100

    Updated globals
    Adding global variables for view elements and API feed URLs.

--- js/common/globals.js
+++ js/common/globals.js
@@ -1,4 +1,14 @@
 // Set the object as the module exports
 module.exports = {
 
+	// View DOM elements
+	elNav: '#nav',
+	elSearch: '#search',
+	elFeed: '#feed',
+
+	// API URLs
+	api: {
+		getFeed: 'feed/'
+	}
+
 };
